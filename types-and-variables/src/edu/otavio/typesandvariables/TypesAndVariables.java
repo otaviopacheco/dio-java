6f2adb5b32db0d5a11b85587e0e88e838bdf6ad1
otaviopacheco <aapo5719@gmail.com>
2023-10-05 16:54:50 -0300

    final commit of types, variables and operators

---
+++ types-and-variables/src/edu/otavio/typesandvariables/TypesAndVariables.java
@@ -0,0 +1,30 @@
+package edu.otavio.typesandvariables;
+
+public class TypesAndVariables {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+        short numeroMinimo = 1;
+        int numeroInteiro = numeroMinimo;
+        // short numerominumo2 = numeroInteiro;
+        // int numero = 1;
+        // System.out.println(numero);
+        // numero = 10;
+        // System.out.println(numero);
+
+        // final double PI = 3.14;
+
+        // int numero = 5;
+        // System.out.println(- numero);
+        // numero = - numero;
+        // System.out.println(+ numero);
+        // numero = numero * -1;
+        // System.out.println(numero);
+
+    int a,b;
+    a=6;
+    b=6;
+        int resultado =a == b? 1:0;
+    System.out.println(resultado);
+    }
+
+}
